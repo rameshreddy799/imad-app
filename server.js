154b08eb07ed049d3aefab3406f9674d2add8e7b
rameshreddy799 <30945624+rameshreddy799@users.noreply.github.com>
2017-08-18 12:29:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req,res){
-    res.send(creteTemplate(articleOne));
+    res.send(createTemplate(articleOne));
 });
 app.get('/article-two', function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
