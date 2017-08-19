b96253ae688590758003efca14def46dd1c7788f
rameshreddy799 <30945624+rameshreddy799@users.noreply.github.com>
2017-08-19 14:59:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,9 +6,9 @@ var marginLeft=0;
 function moveRight()
 {
     marginLeft=marginLeft+5;
-    img.style.mrginLeft=marginLeft+'px';
+    img.style.marginLeft=marginLeft+'px';
 }
 img.onclick = function()
 {
-    var interval=setInterval(moveLeft,50);
+    var interval=setInterval(moveRight,50);
 };
