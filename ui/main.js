3c727aa6eb0726d2ba47b48d9633615aeab0acfd
rameshreddy799 <30945624+rameshreddy799@users.noreply.github.com>
2017-08-20 11:24:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,6 +10,6 @@ button.onclick=function(){
             }
         }
     };
-    request.open('GET' ,'rameshreddy799.imad.hasura-app.io/counter',true);
+    request.open('GET' ,'http://rameshreddy799.imad.hasura-app.io/counter',true);
     request.send(null);
 };
