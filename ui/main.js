3699dbaaeb68fd99e826a0e3d68bdece5ac6ecd2
rameshreddy799 <30945624+rameshreddy799@users.noreply.github.com>
2017-08-21 12:10:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,18 @@
 var button=document.getElementById('counter');
+var counter=0;
 button.onclick=function(){
     var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
         if(request.readystate===XMLHttpRequest.DONE) {
             if(request.status===200){
               var counter= request.responseText;
-              counter=counter+1;
               var span=document.getElementById('count');
               span.innerHTML=counter.toString();
+              
             }
         }
     };
+              
     request.open('GET' ,'http://rameshreddy799.imad.hasura-app.io/counter',true);
     request.send(null);
 };
@@ -18,10 +20,10 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-    var nanes=['name1','name2','name3'];
+    var names=['name1','name2','name3','name4'];
     var list='';
     for(var i=0;i<names.length;i++){
-        list=='<li>'+names[i]+'</li>';
+        list ='<li>'+names[i]+'</li>';
         }
         var ul=document.getElementById('namelist');
         ul.innerHTML=list;
