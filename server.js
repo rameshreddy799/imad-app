064c40f0ff5f4fea4c0db4a5fda445f59554d441
rameshreddy799 <30945624+rameshreddy799@users.noreply.github.com>
2017-08-30 20:46:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,11 +108,22 @@ app.get('/submit-name',function(req,res){
     names.push('name');
     res.send(JSON.stringify(names));
 });
-app.get('/:articleName', function(req,res){
+app.get('/articles/articleName', function(req,res){
     //articleName==article-one
     //articles[articleName]={} content object for article-one
     var articleName =req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+   pool.query("SELECT * FROM article WHERE title= "+req.params.articleName,function(err,result){
+       if(err){
+           err.status(500).send(err.toString());
+       } else {
+           if(result.rows.lenght===0){
+               res.status(404).send('article not found');
+           }else{
+               var articleData=result.rows[0];
+               result.send(createTemplate(articleData));
+           }
+       }
+   });
 });
 
 app.get('/ui/style.css', function (req, res) {
